570a9f617c3fab3a77a2d87bc7f37df1abd003b6
hexu <hexu_1985@sina.com>
2018-01-04 17:14:58 +0800

    add dictionaries

--- plugin/src/dictionary_manager.cpp
+++ plugin/src/dictionary_manager.cpp
@@ -6,31 +6,17 @@ DictionaryManager DictionaryManager::dictionaryManager;
 
 std::vector<std::string> DictionaryManager::getDictionaryPaths() {
     std::vector<std::string> paths;
-    paths.push_back("./german_english.so");
-    paths.push_back("./german_french.so");
+    paths.push_back("./dictionaries/libgerman_english_dictionary.so");
+    paths.push_back("./dictionaries/libgerman_french_dictionary.so");
     return paths;
 }
 
 IDictionary *DictionaryManager::loadDictionary(const std::string &path) {
-    struct DlHandleGuard {
-        void *handle = nullptr;
-
-        DlHandleGuard(void *handle) {
-            this->handle = handle;
-        }
-
-        ~DlHandleGuard() {
-            dlclose(handle);
-        }
-    };
-
     void *handle = dlopen(path.c_str(), RTLD_NOW);
     if (handle == NULL) {
         std::cout << "load " << path << " error: " << dlerror() << std::endl;
         return NULL;
     }
-
-    DlHandleGuard guard(handle);
 
     typedef IDictionary *(*get_dictionary_func_ptr)();
     get_dictionary_func_ptr get_dictionary = (get_dictionary_func_ptr) dlsym(handle, "get_dictionary");
